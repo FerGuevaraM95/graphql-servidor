ee2e50feb140b8af60e1f24744d138f8f39fa703
Fernando Guevara <luisfernandoguevara95@gmail.com>
2019-07-15 21:31:39 -0500

    (Add):Router

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ApolloClient from 'apollo-boost';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // Importar componentes
 import Header from './componentes/Header';
 import Clientes from './componentes/Clientes';
+import NuevoCliente from './componentes/NuevoCliente';
+import EditarCliente from './componentes/EditarCliente';
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
@@ -17,10 +20,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Header />
-      <div className="container">
-      <Clientes />
-      </div>
+      <Router>
+        <Fragment>
+          <Header />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Clientes}/>
+              <Route exact path="/cliente/nuevo" component={NuevoCliente}/>
+              <Route exact path="/cliente/editar/:id" component={EditarCliente}/>
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
     </ApolloProvider>
   );
 }
